11b7afafd564348617cb5b4312935755550603b7
Cecília Maria <cecilia.msaraujo9@gmail.com>
2025-03-20 12:20:26 -0300

    Implementação do formulário de login com estados e evento de submissão

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,61 @@
+import { FaUser, FaLock } from "react-icons/fa";
+
+import { useState } from "react";
+
+import "./Login.css";
+
+const Login = () => {
+  // Estados para armazenar as entradas do usuário
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  // Função que é chamada quando o formulário é enviado
+  const handleSubmit = (event) => {
+    // Impede que a página seja recarregada
+    event.preventDefault();
+
+    alert("Enviando os dados" + username + " - " + password);
+  };
+
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <h1>Acesse o sistemas</h1>
+        <div>
+          <input
+            type="email"
+            placeholder="Email"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <FaUser className="icon" />{" "}
+        </div>
+        <div>
+          <input
+            type="password"
+            placeholder="Senha"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <FaLock className="icon" />
+        </div>
+
+        <div className="recall-forget">
+          <label>
+            <input type="checkbox" />
+            Lembre de mim
+          </label>
+          <a href="">Esqueceu a senha</a>
+        </div>
+
+        <button>Entrar</button>
+
+        <div className="signup-link">
+          <p>
+            Não tem uma conta? <a href="#">Registrar</a>{" "}
+          </p>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
